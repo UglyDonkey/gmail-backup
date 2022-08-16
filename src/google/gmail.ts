4ef63135946516bc25ce83be5f85daba758aa9af
UglyDonkey <uglyd0nk3y@gmail.com>
2022-08-16 21:19:06 +0200

    attachments working

--- src/google/gmail.ts
+++ src/google/gmail.ts
@@ -8,7 +8,10 @@ export type Attachment = gmail_v1.Schema$MessagePartBody
 
 const LIST_PARAMS = {userId: 'me', maxResults: 500}
 
-type MessageReadable = { messages: ReadableStream<Message>, attachments: ReadableStream<Attachment> }
+export interface MessageWithAttachments {
+  message: Message,
+  attachments: Attachment[]
+}
 
 export class Gmail {
   constructor(private gmail: gmail_v1.Gmail) {}
@@ -18,7 +21,7 @@ export class Gmail {
     return profile
   }
 
-  getEmailReader(setTotal?: (total: number) => void, lastMessageId?: string): MessageReadable {
+  getEmailReader(setTotal?: (total: number) => void, lastMessageId?: string): ReadableStream<MessageWithAttachments> {
     let total = 0
     const mailIdsStream = new ReadableStream<string>({
       start: async controller => {
@@ -43,40 +46,35 @@ export class Gmail {
       },
     })
 
-    const mailStream = new TransformStream<string, Message>({
+    const fetchMailsStream = new TransformStream<string, Message>({
       transform: async (messageId, controller) => {
         const {data: message} = await this.gmail.users.messages.get({userId: 'me', id: messageId})
         controller.enqueue(message)
       },
     }, QUEUING_STRATEGY, QUEUING_STRATEGY)
 
-    const extractAttachmentsStream = new TransformStream<Message, { messageId: string, attachmentId: string }>({
-      transform: (message, controller) => {
-        message.payload?.parts
+    const fetchAttachmentsStream = new TransformStream<Message, MessageWithAttachments>({
+      transform: async (message, controller) => {
+        const attachments = await Promise.all(message.payload?.parts
           ?.filter(part =>
-            part.headers?.some(header => header.name === 'Content-Disposition' && header.value === 'attachment'),
+            part.headers?.some(header =>
+              header.name === 'Content-Disposition' && header.value?.startsWith('attachment'),
+            ),
           )
           .map(part => part.body!.attachmentId!)
-          .forEach(attachmentId => controller.enqueue({messageId: message.id!, attachmentId}))
-      },
-    }, QUEUING_STRATEGY, QUEUING_STRATEGY)
-
-    const fetchAttachmentStream = new TransformStream<{ messageId: string, attachmentId: string }, Attachment>({
-      transform: async ({messageId, attachmentId}, controller) => {
-        const {data: attachment} = await this.gmail.users.messages.attachments.get({
-          userId: 'me',
-          messageId,
-          id: attachmentId,
-        })
-        attachment.attachmentId = attachmentId
-        controller.enqueue(attachment)
+          .map(attachmentId => this.gmail.users.messages.attachments.get({
+            userId: 'me',
+            messageId: message.id!,
+            id: attachmentId,
+          }).then(response => ({attachmentId, ...response.data}))) || [])
+        controller.enqueue({message, attachments})
       },
     })
 
-    const mailStreams = mailIdsStream.pipeThrough(reverseStream()).pipeThrough(mailStream).tee()
-    const attachments = mailStreams[0].pipeThrough(extractAttachmentsStream).pipeThrough(fetchAttachmentStream)
-
-    return {messages: mailStreams[1], attachments}
+    return mailIdsStream
+      .pipeThrough(reverseStream())
+      .pipeThrough(fetchMailsStream)
+      .pipeThrough(fetchAttachmentsStream)
   }
 }
 
