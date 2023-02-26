8cf2197504fabbcca80e07d443e7e3557a0c67a7
UglyDonkey <uglyd0nk3y@gmail.com>
2023-02-26 14:01:00 +0100

    fix for @oclif/core@2.3.1, bump other oclif dependencies

--- src/commands/oauth/index.ts
+++ src/commands/oauth/index.ts
@@ -1,4 +1,4 @@
-import {CliUx, Command} from '@oclif/core'
+import {ux, Command} from '@oclif/core'
 import {generateAuthUrl, prepareCredentials} from '../../google/oauth'
 import {saveCredentials} from '../../data/credentials'
 
@@ -8,16 +8,14 @@ export default class OAuth extends Command {
   async run(): Promise<void> {
     const authUrl = generateAuthUrl()
 
-    CliUx.ux.info('Use link below to setup your google account with gmail-backup')
-    CliUx.ux.url(authUrl, authUrl)
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    CliUx.ux.open(authUrl).catch(() => {})
+    ux.info('Use link below to setup your google account with gmail-backup')
+    ux.url(authUrl, authUrl)
 
-    const url: string = await CliUx.ux.prompt('Paste url from browser nav bar', {required: true})
+    const url: string = await ux.prompt('Paste url from browser nav bar', {required: true})
 
     const credentials = await prepareCredentials(url)
     await saveCredentials(credentials)
 
-    CliUx.ux.info('Account successfully added')
+    ux.info('Account successfully added')
   }
 }
--- src/commands/snapshot/create.ts
+++ src/commands/snapshot/create.ts
@@ -1,4 +1,4 @@
-import {CliUx, Command} from '@oclif/core'
+import {Command, ux} from '@oclif/core'
 import {prepareOAuthWithCredentials} from '../../google/oauth'
 import {toGmail} from '../../google/gmail'
 import {findLastSavedMessage, SnapshotBuilder} from '../../data/snapshot'
@@ -12,20 +12,20 @@ export default class CreateSnapshot extends Command {
     const gmails = (await prepareOAuthWithCredentials()).map(toGmail)
     const snapshot = new SnapshotBuilder()
     for (const gmail of gmails) {
-      const progress = CliUx.ux.progress({noTTYOutput: true})
+      const progress = ux.progress({noTTYOutput: true})
 
       const profile = await gmail.getProfile()
       if (!profile.emailAddress) {
         throw new Error('emailAddress must be defined')
       }
-      CliUx.ux.info(`creating snapshot for ${profile.emailAddress}`)
+      ux.info(`creating snapshot for ${profile.emailAddress}`)
 
       const snapshotWriter = await snapshot.getSnapshotWriter(profile.emailAddress, count => progress.update(count))
 
       const labels = await gmail.getLabels()
       await snapshotWriter.writeLabels(labels)
 
-      progress.start()
+      progress.start(0, 0)
 
       const lastSavedMessage = await findLastSavedMessage(profile.emailAddress)
       const emailReader = gmail.getEmailReader(total => progress.setTotal(total), lastSavedMessage?.id ?? undefined)
@@ -34,6 +34,6 @@ export default class CreateSnapshot extends Command {
 
       progress.stop()
     }
-    CliUx.ux.info('snapshot created')
+    ux.info('snapshot created')
   }
 }
